a15d8f968b81b14aa846e100b4bf7907ee8014b7
codmoni <doinghmw@gmail.com>
2024-07-24 14:37:05 +0900

    'Feat:moni3'

---
+++ src/components/history(moni)/ContentTitle.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+import '../../pages/moni/history.css'
+
+const Container = styled.div`
+    position:relative;
+    margin-top:10px;
+    margin-bottom:33px;
+`
+const ContentTitle =({text})=>{
+    return(
+        <Container>
+            <h1 style={{display:'inline-block'}}>{text}</h1>
+            <p className='lastUpdated' style={{display:'inline-block', position:'absolute', top:'10px', right:0}}>마지막 수정일시: 2022-02-02 00:00</p>
+        </Container>
+    )
+}
+
+export default ContentTitle;
