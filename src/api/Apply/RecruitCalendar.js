0cd2dfe34595e8e6e18fe717efe52452f6928d47
yeondub1121 <kssjshyjj@naver.com>
2024-08-14 01:26:27 +0900

    Fix:

--- src/api/Apply/RecruitCalendar.js
+++ src/api/Apply/RecruitCalendar.js
@@ -1,13 +1,12 @@
 export const getRecruitCalendar = async (year, month) => {
-    try {
-      const response = await fetch(`https://api.kkijuk.com/recruit/calendar?year=${year}&month=${month}`);
-      if (!response.ok) {
-        throw new Error('Failed to fetch recruit calendar data');
-      }
-      return await response.json();
-    } catch (error) {
-      console.error('Error fetching recruit calendar data:', error);
-      throw error;
+  try {
+    const response = await fetch(`https://api.kkijuk.com/recruit/calendar?year=${year}&month=${month}`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch recruit calendar data');
     }
-  };
-  
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching recruit calendar data:', error);
+    throw error;
+  }
+};
