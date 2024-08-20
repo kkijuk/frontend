36ccee0ddbea04b376ac9f720cb0a382e5485ab2
yeondub <114345405+yeondub1121@users.noreply.github.com>
2024-08-21 01:42:08 +0900

    Revert "Moni"

---
+++ src/pages/Apply.jsx
@@ -0,0 +1,6 @@
+export default function Apply() {
+
+    return (
+        <div>지원관리</div> // 코드 작성 시 삭제해주세요!
+    )
+}
--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -1,36 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
+export default function Community() {
 
-    const Communitycontainer = styled.div`
-    max-width: flex;
-    margin: 50px auto;
-    padding: 20px;
-    background: white;
-    border-radius: 10px;
-    text-align: center;
-    margin-bottom: 70px;
-    position: relative;
-    top: 100px; 
-  
-    p {
-      margin-top: 10px;
-      color: var(--main-01, #3AAF85);
-      text-align: center;
-      font-family: REgular;
-      font-size: 27px;
-      font-style: normal;
-      font-weight: 700;
-      line-height: normal;
-    }
-  `;
-  
-  const  Community = () => {
     return (
-      <Communitycontainer>
-        <p>준비중입니다</p>
-        <p>업데이트 될 끼적을 기대해주세요!</p>
-      </Communitycontainer>
-    );
-  };
-  
-  export default Community
+        <div>커뮤니티</div> // 코드 작성 시 삭제해주세요!
+    )
+}
