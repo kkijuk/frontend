f281499d2f5244dd2191d644337b5072d7dec56c
codmoni <doinghmw@gmail.com>
2024-08-22 19:12:47 +0900

    'Feat:api_0822'

---
+++ src/api/Signup/createResume.js
@@ -0,0 +1,23 @@
+import api from "../../Axios";
+
+const createResume =()=>{
+    try{
+        const response = api.post('/history/resume');
+        console.log('이력서 생성: ', response.data);
+
+    }catch(error){
+        console.error('Erraor creating education:', error);
+        if (error.response) {
+            // 서버 응답이 있는 경우
+            console.error('Server responded with:', error.response.data);
+        } else if (error.request) {
+            // 요청이 보내졌지만 응답을 받지 못한 경우
+            console.error('No response received:', error.request);
+        } else {
+            // 요청 설정 중에 오류가 발생한 경우
+            console.error('Error setting up request:', error.message);
+        }
+    }
+}
+
+export default createResume;
