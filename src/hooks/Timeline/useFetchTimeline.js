214e6118e348cf36abb97a112a2ba16f4c7ee10b
kosssshhhh <kosukwhon@gmail.com>
2025-01-20 15:22:45 +0900

    🔧 Fix: 타임라인 api 오류 수정

--- src/hooks/Timeline/useFetchTimeline.js
+++ src/hooks/Timeline/useFetchTimeline.js
@@ -5,10 +5,11 @@ import { getTimeline } from '../../api/Timeline/getTimeline';
 export const useFetchTimeline = () => {
 	const { data, isLoading, error } = useQuery({
 		queryKey: 'timeline',
-		queryFn: () => getTimeline(),
+		queryFn: getTimeline,
 	});
 
-	console.log('data', data);
+	// data가 존재하면 data.data만 반환
+	const processedData = data?.data || [];
 
-	return { data, isLoading, error };
+	return { data: processedData, isLoading, error };
 };
