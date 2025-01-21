008c2b65f527fd3ab37f60e6de84a538017218af
cinsy26 <siusy2618@naver.com>
2025-01-21 20:20:18 +0900

    Feat: mypage

--- src/pages/Mypage/AuthenticationAccount.jsx
+++ src/pages/Mypage/AuthenticationAccount.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SubNav2 from '../../components/Mypage/SubNav2';
 import Authentication from '../../components/Mypage/Authentication';
 
+//이제 안씀~//
 export default function AuthenticationAccount() {
 	return (
 		<div>
