ce89eea957fe747c92e2c54c2c49a04db0d5db60
cinsy26 <siusy2618@naver.com>
2025-01-23 07:19:27 +0900

    Feat: Mypage design 완료

--- src/pages/Mypage/Confirm.jsx
+++ src/pages/Mypage/Confirm.jsx
@@ -14,20 +14,17 @@ import { fetchEmail } from '../../api/Mypage/MyinformationVerify';
 
 const Container = styled.div`
 	width: 820px;
-	height: 300px;
 	border: 1px solid black;
-`;
-
-const Box = styled.div`
-	/* props로 height 설정 */
-	width: 820px;
-	height: ${(props) => props.height || '100px'}; /* 기본값은 100px */
-	border: 1px solid black;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	margin: 0 auto;
 `;
 
 const Text = styled.div`
 	/*설명 텍스트*/
-	width: 420px;
+	width: 500px;
 	height: 19px;
 	color: var(--black, #000);
 	text-align: center;
@@ -36,40 +33,44 @@ const Text = styled.div`
 	font-style: normal;
 	font-weight: 400;
 	line-height: normal;
-	border: 1px solid black;
+
+	margin-top: 100px;
 `;
 
 const EmailBox = styled.div`
 	width: 400px;
 	height: 81px;
-	border: 1px solid black;
+
+	margin-top: 56px;
 `;
 
 const EmailTextBox = styled.div`
 	width: 400px;
 	height: 31px;
 	gap: 20px;
-	border: 1px solid black;
+
+	display: flex;
+	align-items: center;
 `;
 
 const EmailText = styled.div`
+	/*이메일 글씨*/
 	color: var(--main-01, #3aaf85);
 	font-family: Pretendard;
 	font-size: 18px;
 	font-style: normal;
 	font-weight: 500;
 	line-height: normal;
-	border: 1px solid black;
 `;
 
 const Email = styled.div`
+	/*이메일 가린거 보여주기*/
 	color: var(--gray-02, #707070);
 	font-family: Pretendard;
 	font-size: 16px;
 	font-style: normal;
 	font-weight: 400;
 	line-height: normal;
-	border: 1px solid black;
 `;
 
 const KakaoTag = styled.div`
@@ -84,14 +85,15 @@ const KakaoTag = styled.div`
 	font-size: 14px;
 	font-style: normal;
 	font-weight: 500;
-	line-height: normal;
-	border: 1px solid black;
+	line-height: 25px;
 `;
 
 const Input = styled.input`
 	width: 400px;
 	height: 50px;
-
+	padding-left: 20px;
+	border: none; /* 테두리 제거 */
+	outline: none; /* 포커스 시 라인 제거 */
 	border-radius: 10px;
 	background: #f5f5f5;
 
@@ -109,6 +111,8 @@ const Button = styled.button`
 	border-radius: 10px;
 	background: var(--main-01, #3aaf85);
 
+	border: none; /* 테두리 제거 */
+	outline: none; /* 포커스 시 라인 제거 */
 	color: #fff;
 	text-align: center;
 	font-family: Pretendard;
@@ -116,6 +120,8 @@ const Button = styled.button`
 	font-style: normal;
 	font-weight: 500;
 	line-height: normal;
+
+	margin-top: 50px;
 `;
 
 export default function AuthenticationAccount() {
@@ -146,21 +152,18 @@ export default function AuthenticationAccount() {
 	return (
 		<div>
 			<SubNav2></SubNav2>
+
 			<Container>
-				<Box height={100}></Box>
 				<Text>회원님의 정보를 보호하기 위해 가입한 이메일 재입력이 필요합니다.</Text>
-				<Box height={56}></Box>
 				<EmailBox>
 					<EmailTextBox>
 						<EmailText>이메일</EmailText>
-						<Email></Email>
+						<Email>k*****ij.@naver.com</Email>
 						<KakaoTag>카카오</KakaoTag>
 					</EmailTextBox>
-					<Input placeholder="이메일을 입력하세요" value={inputEmail} onChange={handleInputChange} />{' '}
+					<Input placeholder="이메일을 입력하세요" />
 				</EmailBox>
-				<Box height={50}></Box>
-				<Button onclick={handleSubmit}>확인</Button>
-				{errorMessage && <Text style={{ color: 'red' }}>{errorMessage}</Text>}
+				<Button>확인</Button>
 			</Container>
 		</div>
 	);
