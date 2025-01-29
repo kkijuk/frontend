ba37f9a72b16f4fbde18824817d18af6f629bb1d
kosssshhhh <kosukwhon@gmail.com>
2025-01-29 15:19:00 +0900

    🔧 Fix: 내커리어 예외 컴포넌트 UI 수정

--- src/components/Mycareer/EmptyActivityMessage.jsx
+++ src/components/Mycareer/EmptyActivityMessage.jsx
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 
 const MessageContainer = styled.div`
 	display: flex;
+	flex-direction: column;
 	justify-content: center;
 	align-items: center;
-	height: 100%;
+	margin-top: 80px;
 	color: var(--gray-01, #424242);
 	text-align: center;
 `;
