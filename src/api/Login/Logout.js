e8158be4581aedc72d671bb96186ec4dee7e5792
yeondub1121 <kssjshyjj@naver.com>
2025-02-06 18:19:38 +0900

    Feat: 인터셉터에 로그아웃 로직 추가

--- src/api/Login/Logout.js
+++ src/api/Login/Logout.js
@@ -1,30 +1,27 @@
-export async function logout() {
-	const apiUrl = `${process.env.REACT_APP_API_URL}/logout`;
+import axios from 'axios';
 
-	try {
-		const response = await fetch(apiUrl, {
-			method: 'POST',
-			headers: {
-				accept: '*/*',
-			},
-			credentials: 'include',
-		});
+export const logout = async () => {
+    try {
+        const response = await api.post('/logout', null, {
+            headers: {
+                accept: '*/*',
+            },
+        });
 
-		if (response.ok) {
-			const resultText = await response.text();
+        if (response.status === 200) {
+            const resultText = response.data;
 
-			if (resultText === 'logout success') {
-				console.log('로그아웃 성공:', resultText);
-				return true;
-			} else {
-				throw new Error('Unexpected response format');
-			}
-		} else {
-			const errorData = await response.json();
-			throw new Error(errorData.message || '로그아웃 중 오류가 발생했습니다.');
-		}
-	} catch (error) {
-		console.error('로그아웃 오류:', error.message);
-		throw new Error(error.message);
-	}
-}
+            if (resultText === 'logout success') {
+                console.log('로그아웃 성공:', resultText);
+                return true;
+            } else {
+                throw new Error('Unexpected response format');
+            }
+        } else {
+            throw new Error('로그아웃 중 오류가 발생했습니다.');
+        }
+    } catch (error) {
+        console.error('로그아웃 오류:', error.message);
+        throw new Error(error.message);
+    }
+};
