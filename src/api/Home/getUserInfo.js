81842b56686635d94b23000ce67855c2ecce1b74
yeondub1121 <kssjshyjj@naver.com>
2025-02-06 21:49:47 +0900

    Fix: 홈 api 수정

--- src/api/Home/getUserInfo.js
+++ src/api/Home/getUserInfo.js
@@ -1,12 +1,16 @@
 import api from '../../Axios';
 
-export const getUserInfo = async () => {
-	try {
-		const response = await api.get('/dashboard/user-info', {
-			withCredentials: true, // 요청에 쿠키 포함
-		});
-	} catch (error) {
-		console.error('Error fetching user info:', error.message);
-		return null;
-	}
+export const getUserInfo = async (token) => {
+    try {
+        const response = await api.get('/dashboard/user-info', {
+            headers: {
+                Authorization: `Bearer ${token}`, // 요청 헤더에 토큰 추가
+            },
+        });
+        console.log('사용자 정보:', response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching user info:', error.message);
+        return null;
+    }
 };
