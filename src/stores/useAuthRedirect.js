9f9da876afb965c09348503cb75a3a958b5b4f6f
yeondub1121 <kssjshyjj@naver.com>
2025-02-07 14:14:34 +0900

    Feat: 로그아웃 에러 처리

--- src/stores/useAuthRedirect.js
+++ src/stores/useAuthRedirect.js
@@ -1,16 +1,20 @@
 import { useEffect } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import useAuthStore from '../stores/useAuthStore';
 
 const useAuthRedirect = () => {
     const navigate = useNavigate();
     const { isLoggedIn } = useAuthStore();
-
+    
     useEffect(() => {
-        const path = window.location.pathname; //  location.pathname 직접 가져오기
+        const path = window.location.pathname; //  현재 경로 가져오기
+        const isManualLogout = sessionStorage.getItem('manualLogout'); // ✅ 직접 로그아웃했는지 확인
 
         if (!isLoggedIn) {
-            alert('로그인이 만료되었습니다. 다시 로그인해주세요.');
+            if (!isManualLogout) {
+                alert('로그인이 만료되었습니다. 다시 로그인해주세요.');
+            }
+            sessionStorage.removeItem('manualLogout'); //  메시지 한 번만 뜨게 초기화
             navigate('/');
         } else if (path === '/') {
             navigate('/home');
