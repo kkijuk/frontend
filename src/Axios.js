efabb0f55ae2f5996b4ecacaa11a7ef9e26466f1
yeondub1121 <kssjshyjj@naver.com>
2025-02-11 15:37:44 +0900

    Feat: edge 에러 개선

--- src/Axios.js
+++ src/Axios.js
@@ -10,6 +10,7 @@ const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
     headers: {
         'Content-Type': 'application/json',
+        'Origin': window.location.origin,
     },
     withCredentials: true,
 });
