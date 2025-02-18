34facbc85473f189f4cb25c168d0d5e971ccbb71
yeondub1121 <kssjshyjj@naver.com>
2025-02-18 14:35:04 +0900

    Remove: 브라우저 파일 삭제

--- src/App.js
+++ src/App.js
@@ -47,10 +47,8 @@ import PrivacyAgreed from './pages/PrivacyAgreed';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import FilterPage from './components/Apply/FilterPage';
-
 import Error from './pages/Error/BasicError';
 import NumError from './pages/Error/NumError';
-import BrowserError from './pages/Error/BrowserError';
 
 const AppContainer = styled.div`
 	display: flex;
@@ -146,7 +144,6 @@ const App = () => {
 					<Route path="/mypage/resetsuccess" element={<ResetSuccess />} />
 					<Route path="/error" element={<Error />} />
 					<Route path="/numerror" element={<NumError />} />
-					<Route path="/browsererror" element={<BrowserError />} />
 				</Routes>
 			</MainContent>
 			{!hideHeaderFooter && <Footer />}
