7b871dde952bebc1a246ed37638c8914a07a7d53
codmoni <doinghmw@gmail.com>
2025-03-01 03:53:35 +0900

    refactor: 학력 입력 제한 예외처리 추가

--- src/components/Record/Profile.jsx
+++ src/components/Record/Profile.jsx
@@ -37,10 +37,24 @@ const Profile = ({ profileBlob, onProfileChange }) => {
 
     return (
         <ProfileContainer onClick={handleProfileClick}>
+            <HiddenSymbolArea>
+                <svg width="0" height="0">
+                    <symbol fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 45 46" id="Vector">
+                        <g id="Vector">
+                        <path d="M45 23C45 35.4264 34.9264 45.5 22.5 45.5C10.0736 45.5 0 35.4264 0 23C0 10.5736 10.0736 0.5 22.5 0.5C34.9264 0.5 45 10.5736 45 23Z" fill="#D9D9D9"></path>
+                        <path d="M20.5469 25.4062H14.7031V21.5H20.5469V15.6562H24.4531V21.5H30.2969V25.4062H24.4531V31.25H20.5469V25.4062Z" fill="white"></path>
+                        </g>
+                    </symbol>
+                </svg>
+            </HiddenSymbolArea>
             {profileUrl ? (
                 <ProfileImage src={profileUrl} alt="Profile" />
             ) : (
-                <Placeholder />
+                <Placeholder>
+                    <SvgPlaceholder>
+                        <use href="#Vector" />
+                    </SvgPlaceholder>
+                </Placeholder>
             )}
             <input
                 type="file"
@@ -73,4 +87,18 @@ const Placeholder = styled.div`
     height: 200px;
     flex-shrink: 0;
     background: var(--gray-05, #F1F1F1);
+`;
+
+const HiddenSymbolArea = styled.svg`
+    width: 0;
+    height: 0;
+    display: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const SvgPlaceholder = styled.svg`
+    width: 45px;
+    height: 45px;
 `;
