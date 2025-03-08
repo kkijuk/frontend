2ae58fdace185779a43d60230a7f06a723ce6581
codmoni <doinghmw@gmail.com>
2025-03-08 21:13:04 +0900

    fix: CareerItem

--- src/components/Record/CareerItem.jsx
+++ src/components/Record/CareerItem.jsx
@@ -163,7 +163,7 @@ const CareerItem = ({ data, isLastItem, onEditCareer }) => {
 							<Status>{statusToDisplay}</Status>
 						</Dates>
 						<DetailContainer>
-							<div style={{ width:'58px',fontWeight: '600', marginRight: '30px'}}>활동내역</div>
+							<DetailLabel>활동내역</DetailLabel>
 							{isSummaryEditMode ? (
 								<DetailWrapper>
 									<DetailTextArea 
@@ -383,6 +383,13 @@ const DetailContainer = styled.div`
 	flex-direction: row;
 	margin-top: 10px;
 `;
+
+const DetailLabel = styled.div`
+	width: 58px;
+	font-weight: 600;
+	margin-right: 30px;
+	flex-shirnk: 0;
+`
 
 const DetailWrapper = styled.div`
 	display: flex;
