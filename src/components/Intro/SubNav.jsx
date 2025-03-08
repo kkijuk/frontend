088d6c48fac9ec0fb8cb6fbe63473f69351eca53
cinsy26 <siusy2618@naver.com>
2025-03-09 00:14:41 +0900

    FIX: QA mycareer text style, home writing noti

--- src/components/Intro/SubNav.jsx
+++ src/components/Intro/SubNav.jsx
@@ -17,9 +17,7 @@ const SubNav = () => {
 		<Layout title="서류준비">
 			<BaseDiv>
 				{isResumeActive && (
-					<ExportButton onClick = {()=>navigate('/history/resumeExport')}>
-						문서로 내보내기
-					</ExportButton>
+					<ExportButton onClick={() => alert('이 페이지는 준비중입니다.')}>문서로 내보내기</ExportButton>
 				)}
 				<Nav>
 					<NavItems onClick={() => navigate('/history')} active={isResumeActive}>
@@ -35,8 +33,7 @@ const SubNav = () => {
 					<Section>
 						<Outlet />
 					</Section>
-					
-				</Nav>		
+				</Nav>
 			</BaseDiv>
 		</Layout>
 	);
@@ -79,18 +76,18 @@ const Linear = styled.div`
 
 const Section = styled.div`
 	height: 100%;
-`
+`;
 const ExportButton = styled.button`
 	width: 150px;
 	height: 35px;
 	flex-shrink: 0;
 	border-radius: 10px;
 	border: none;
-	background: var(--main-01, #3AAF85);
+	background: var(--main-01, #3aaf85);
 	position: absolute;
 	right: 0;
 	top: -50px;
-	color: var(--white, #FFF);
+	color: var(--white, #fff);
 	text-align: center;
 	font-family: Regular;
 	font-size: 18px;
@@ -98,4 +95,4 @@ const ExportButton = styled.button`
 	font-weight: 500;
 	line-height: normal;
 	cursor: pointer;
-`
+`;
