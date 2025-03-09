84553ccfa7fb77d1447de560432ec1f0eeeadd33
codmoni <doinghmw@gmail.com>
2025-03-09 18:12:44 +0900

    fix: s3File.js

--- src/api/Record/s3File.js
+++ src/api/Record/s3File.js
@@ -32,7 +32,7 @@ const uploadFileToS3 = async (file, presignedURL) => {
     try{
         const response = await axios.put(presignedURL, file, {
             headers: {
-                'Content-Type': file.type || 'application/pdf',
+                'Content-Type': file.type,
                 'x-amz-server-side-encryption' : 'AES256'
             }
         });
