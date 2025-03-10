342d8aef4bf8bc657762aa1ed60d097e84b2dbe3
codmoni <doinghmw@gmail.com>
2025-03-11 01:51:22 +0900

    fix: user

--- src/api/Record/user.js
+++ src/api/Record/user.js
@@ -7,7 +7,7 @@ const updateUserData = async (recordId, data) => {
         data.profileImageUrl = data.profileImageUrl ?? "";
         
         console.log("Data to update: ", data);
-        const response = await api.patch(`/history/resume/${recordId}`, data);
+        const response = await api.patch(`/history/resume?recordId=${recordId}`, data);
         console.log("User data updated successfully: ", response.data);
         return response.data;
     } catch (error) {
