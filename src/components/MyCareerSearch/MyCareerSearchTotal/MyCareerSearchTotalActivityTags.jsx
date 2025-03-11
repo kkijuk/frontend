1f773921a9d398bd339835d7fb53611afea9aaef
kosssshhhh <kosukwhon@gmail.com>
2025-03-11 12:51:37 +0900

    🎨 Design: 내커리어 검색 전체 태그 UI 수정 완

--- src/components/MyCareerSearch/MyCareerSearchTotal/MyCareerSearchTotalActivityTags.jsx
+++ src/components/MyCareerSearch/MyCareerSearchTotal/MyCareerSearchTotalActivityTags.jsx
@@ -27,7 +27,7 @@ const TagWrapper = styled.div`
 
 // 태그 버튼
 const Tag = styled.button`
-	padding: 4px 20px;
+	padding: 0 20px;
 	border-radius: 20px;
 	font-family: Pretendard;
 	font-size: 14px;
@@ -59,8 +59,8 @@ const ActivityItem = styled.div`
 	border-bottom: 1px solid #eaeaea;
 	display: flex;
 	flex-direction: column;
-	margin: 0 24px;
-	padding: 16px 0;
+	margin: 0;
+	padding: 24px 0;
 
 	&:last-child {
 		border-bottom: none;
