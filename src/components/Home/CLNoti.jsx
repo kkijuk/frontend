6168ee5f9f2c36bdf9146b970364533d3fdc1c02
cinsy26 <siusy2618@naver.com>
2025-03-24 00:19:22 +0900

    Fix: Home error

--- src/components/Home/CLNoti.jsx
+++ src/components/Home/CLNoti.jsx
@@ -116,8 +116,8 @@ export default function CLNoti() {
 				);
 			}
 
-			const ddayNumber = parseInt(item.dday?.replace('D-', ''));
-			const isDanger = !isNaN(ddayNumber) && ddayNumber <= 7;
+			const ddayNumber = item.dday; // 숫자 그대로 사용
+			const isDanger = ddayNumber <= 7;
 
 			return (
 				<List key={index}>
