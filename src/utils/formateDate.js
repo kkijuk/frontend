9f714bb4020d9302519b31bb628d68f13a6d34ae
kosssshhhh <kosukwhon@gmail.com>
2025-04-03 15:04:37 +0900

    ✨ Feat: 날짜 포맷팅 함수 개선 및 새로운 날짜 변환 함수 추가

--- src/utils/formateDate.js
+++ src/utils/formateDate.js
@@ -1,6 +1,16 @@
 export const formatDate = (startdate, enddate, unknown) => {
+	const formattedStartDate = formateDateDashToDot(startdate);
+	const formattedEndDate = formateDateDashToDot(enddate);
+
 	if (unknown === true) {
-		return `${startdate} ~ ing`;
+		return `${formattedStartDate} ~ ing`;
 	}
-	return startdate === enddate ? startdate : `${startdate} ~ ${enddate}`;
+	return startdate === enddate ? formattedStartDate : `${formattedStartDate} ~ ${formattedEndDate}`;
+};
+
+export const formateDateDashToDot = (dateString) => {
+	if (!dateString) return '';
+
+	// YYYY-MM-DD 형식을 YYYY.MM.DD 형식으로 변환
+	return dateString.replace(/-/g, '.');
 };
