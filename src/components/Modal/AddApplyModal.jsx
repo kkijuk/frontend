e0722413527a35a614613bbd5fd17bdf76a9f0c7
yeondub1121 <kssjshyjj@naver.com>
2025-05-06 03:47:44 +0900

    Feat: 날짜 포맷 수정

--- src/components/Modal/AddApplyModal.jsx
+++ src/components/Modal/AddApplyModal.jsx
@@ -455,7 +455,7 @@ const AddApplyModal = ({ onClose, onSave }) => {
 			const day = String(date.getDate()).padStart(2, '0');
 			const hours = String(date.getHours()).padStart(2, '0');
 			const minutes = String(date.getMinutes()).padStart(2, '0');
-			return `${year}-${month}-${day} ${hours}:${minutes}`;
+			return `${year}.${month}.${day} ${hours}:${minutes}`;
 		};
 
 		const formattedStartTime = formatDateTime(startTime);
