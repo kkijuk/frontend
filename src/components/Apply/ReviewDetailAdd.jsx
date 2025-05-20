e8fa50b08b741ebecd466b8c4d6a9b4fcebb5274
yeondub1121 <kssjshyjj@naver.com>
2025-05-20 20:18:20 +0900

    Fix: 버튼 수정

--- src/components/Apply/ReviewDetailAdd.jsx
+++ src/components/Apply/ReviewDetailAdd.jsx
@@ -38,6 +38,7 @@ const Button = styled.div`
     height: 50px;
     display: flex;
     gap: 15px;
+    margin-top: 20px;
     margin-bottom: 24px;
      @media (max-width: ${({ theme }) => theme.breakpoints.md}) {
     justify-content: center;
