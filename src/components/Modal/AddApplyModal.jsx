b7b15c7f88b64b50464f4a6ead1fa6827693a5b9
yeondub1121 <kssjshyjj@naver.com>
2025-05-20 19:36:09 +0900

    Feat: 높이 expand

--- src/components/Modal/AddApplyModal.jsx
+++ src/components/Modal/AddApplyModal.jsx
@@ -1,6 +1,6 @@
 //pages/Apply/ApplySchedule, Intro/Select
 //공고 추가 모달
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import ModalTagBox from '../Apply/ModalTagBox';  // 일관성 있게 사용
 import { createRecruit } from '../../api/Apply/Recruit';
@@ -455,7 +455,7 @@ const AddApplyModal = ({ onClose, onSave }) => {
 			const day = String(date.getDate()).padStart(2, '0');
 			const hours = String(date.getHours()).padStart(2, '0');
 			const minutes = String(date.getMinutes()).padStart(2, '0');
-			return `${year}.${month}.${day} ${hours}:${minutes}`;
+			return `${year}-${month}-${day} ${hours}:${minutes}`;
 		};
 
 		const formattedStartTime = formatDateTime(startTime);
@@ -568,4 +568,4 @@ const AddApplyModal = ({ onClose, onSave }) => {
 	);
 };
 
-export default AddApplyModal;
+export default AddApplyModal; 
