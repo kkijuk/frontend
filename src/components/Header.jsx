2db035a5ade09969149220d1a65501993a67c63b
yeondub1121 <kssjshyjj@naver.com>
2025-06-01 19:16:26 +0900

    Design: ui 깨진거 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -131,9 +131,9 @@ const DropdownMenu = styled.div`
 	border: 1px solid #ccc;
 	border-radius: 10px;
 	box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
-	width: 110px;
-	height: 70px;
-	padding: 10px;
+	width: 125px;
+	height: 85px;
+	padding: 8px;
 	z-index: 100;
 
 	display: flex;
@@ -145,7 +145,7 @@ const DropdownMenu = styled.div`
 		color: #000;
 		font-family: Regular;
 		font-size: 16px;
-		padding: 8px 0;
+		padding: 7px 0;
 		width: 100%;
 		text-align: center;
 	}
