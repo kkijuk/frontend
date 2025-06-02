46a9f62d2a06b7b55e5f1bd3ce571925bde5b7e3
kosssshhhh <kosukwhon@gmail.com>
2025-06-02 13:26:30 +0900

    🔧 Refactor: AppWrapper의 import 경로를 절대 경로로 변경

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import AppWrapper from './AppWrapper';
+import AppWrapper from '@/AppWrapper';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<AppWrapper />);
