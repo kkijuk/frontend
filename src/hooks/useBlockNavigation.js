584d8c2b67ccdff150c3f5c92506c2dfcb192e7b
cinsy26 <siusy2618@naver.com>
2025-07-21 20:56:09 +0900

    Feat: useBeforUnload 추가

--- src/hooks/useBlockNavigation.js
+++ src/hooks/useBlockNavigation.js
@@ -1,4 +1,3 @@
-// useBlockNavigation.js
 import { useContext, useEffect } from 'react';
 import { UNSAFE_NavigationContext as NavigationContext } from 'react-router-dom';
 
@@ -6,12 +5,10 @@ export function useBlockNavigation(shouldBlock, onNavigate) {
 	const navigator = useContext(NavigationContext).navigator;
 
 	useEffect(() => {
-		console.log('useBlockNavigation called, shouldBlock:', shouldBlock);
 		if (!shouldBlock || typeof navigator.block !== 'function') return;
 
 		const unblock = navigator.block((tx) => {
-			console.log('🛑 Navigation blocked:', tx);
-			onNavigate(tx); // tx.retry() 저장해놨다가 나중에 실행
+			onNavigate(tx); // tx.retry 저장 → onConfirm에서 실행
 		});
 
 		return unblock;
