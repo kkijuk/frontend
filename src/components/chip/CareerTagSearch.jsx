6c4a9a876027b8d12cb706442ca28288b2e66501
codmoni <doinghmw@gmail.com>
2025-08-13 06:47:32 +0900

    Design: chip- CareerTagSearch 구현

---
+++ src/components/chip/CareerTagSearch.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import { Color } from "@/constants/color";
+import { TagContainer } from "./careerTag.styles";
+
+const CareerTagSearch = ({tag, surface = 'white', isSelected = false, onClick}) => {
+    return (
+        <SearchTagContainer
+            $surface={surface}
+            $selected={isSelected}
+            onClick={onClick}
+        >
+            {tag}
+        </SearchTagContainer>
+    );
+};
+
+export default CareerTagSearch;
+
+const SearchTagContainer = styled(TagContainer)`
+    &:hover {
+        background-color: ${Color.gray05};
+    }
+`;
